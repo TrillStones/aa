05fd8d4cefa1b5cb4c4eced60694dc9b18567d49
陈俊杰(002336) <32927876+TrillStones@users.noreply.github.com>
2023-04-05 19:21:33 +0800

    --

--- a.java
+++ a.java
@@ -2,8 +2,5 @@ public class a{
     public static void main(String[] args){
         System.out.println("Hello World");
     }
-
-    public void a(){
-        System.out.println("Hello World");
-    }
+ 
 }
